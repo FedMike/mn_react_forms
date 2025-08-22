9aa7fda9529afca3b3a9408137b804578be27bba
fedmike <molnek87@mail.ru>
2025-08-22 21:50:54 +0900

    updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,18 @@ class Form extends React.Component {
     state = {
         firstName: '',
         email: '',
+        message: '',
+        select: '',
+        subscription: false,
+        gender: '',
     }
 
     handleChange = (event) => {
         this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleCheckboxChange = (event) => {
+        this.setState({[event.target.name]: event.target.checked})
     }
     
     validateName = () => {
@@ -16,14 +24,14 @@ class Form extends React.Component {
         }
     }
 
-    validateEmail = (mail) => {
+    validateEmail = () => {
         if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email)) {
             alert('email is not valid')
         }
     }
 
     render() {
-        const {firstName, email} = this.state
+        const {firstName, email, message, select, subscription, gender} = this.state
         return <div>
             <input 
                 type="text" 
@@ -41,6 +49,28 @@ class Form extends React.Component {
                 onChange={this.handleChange}
                 onBlur={this.validateEmail}
             />
+            <br/>
+            <textarea name="message" value={message} onChange={this.handleChange}></textarea>
+            <br/>
+            <select name="select" value={select} onChange={this.handleChange}>
+                <option value="" disabled></option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+            <br/>
+            <label>
+                <input 
+                    type="checkbox" 
+                    name="subscription" 
+                    checked={subscription}
+                    onChange={this.handleCheckboxChange}
+                />
+                Subcription
+            </label>
+            <br/>
+            <input type="radio" name="gender" value="male" onChange={this.handleChange} checked={gender === "male"}/>Male
+            <input type="radio" name="gender" value="female" onChange={this.handleChange} checked={gender === "female"}/>Female
         </div>
     }
 };
